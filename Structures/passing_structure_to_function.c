c41a1f46067f64ae48dbd6fafc5e71c7a91083a9
Pooja Chothani <poojachothani.187@gmail.com>
2026-04-11 18:27:42 -0400

    c structures

--- Structures/passing_structure_to_function.c
+++ Structures/passing_structure_to_function.c
@@ -8,18 +8,28 @@ struct student{
 };
 
 void print_data(struct student s1);
+void by_pointer(struct student *ptr);
 
 int main()
 {
     struct student s1 = {1, "pooja", 9};
     print_data(s1);
-    printf("%s \n", s1.name);
+    printf("%d \n", s1.roll_no);
+
+    by_pointer(&s1);
+    printf("%d \n", s1.roll_no);
+    
     return 0;
 }
 
 // here struct parameter pass as value not as reference
 void print_data(struct student s1)
 {
-    strcpy(s1.name, "lata");
-    printf("%s \n", s1.name);
+    s1.roll_no = 11;
+    printf("%d \n", s1.roll_no);
+}
+
+void by_pointer(struct student *ptr)
+{
+    (*ptr).roll_no = 10;
 }
